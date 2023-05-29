1933bf0f7cb17548fc3b2d80e4fe6d4795d50f8d
Tolya Leonchik <leonchik_tolik@mail.ru>
2023-05-29 22:06:08 +0300

    commit

--- src/main/java/Bootstrap.java
+++ src/main/java/Bootstrap.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Bootstrap {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+@WebServlet("/site")
+public class Bootstrap extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Success");
+    }
 }
